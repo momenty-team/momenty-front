75f395f4a8f7c7aaac52a712e1ff862618bd4ab2
hyejun <hyejunkkim228@gmail.com>
2025-02-03 15:27:35 +0900

    feat: 폰트 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import '@/styles/globals.css';
-import type { AppProps } from 'next/app';
 import localFont from 'next/font/local';
 
 export const metadata = {
@@ -8,19 +7,14 @@ export const metadata = {
   viewport: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no',
 };
 
-// const suitVariable = localFont({
-//   src: '../assets/fonts/SUIT-Variable.woff2',
-//   variable: '--font-suit',
-// });
+const suitVariable = localFont({
+  src: '../assets/fonts/SUIT-Variable.woff2',
+  variable: '--font-suit',
+});
 
-export default function App({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="ko">
-      <head>
-        <meta name="theme-color" content="#ffffff" />
-        <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-      </head>
+    <html lang="ko" className={suitVariable.variable}>
       <body>{children}</body>
     </html>
   );
