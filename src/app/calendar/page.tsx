5cd508e743e8eb1d48e259c4acc3aabcd12c4a17
d0dam <smallkdb@gmail.com>
2025-02-19 19:18:43 +0900

    feat: 캘린터 라우팅

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,48 +1,45 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
 import useCalendar from './hooks/useCalendar';
 import DateBox from './DateBox';
 import MonthPicker from './MonthPicker';
+import TopNavigation from '@/components/TopNavigation';
+import { postMessageToWebView } from '@/utils';
 
 const DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 function Calendar() {
-  const router = useRouter();
   const { dateList, year, month, setYear, setMonth, isCurrentMonth, isToday, isSunday } = useCalendar();
 
-  const onClickHandler = () => {
-    router.push('/');
+  const routeBack = () => {
+    postMessageToWebView({ route: 'goBack' });
   };
 
   return (
-    <div className="flex flex-col gap-6">
-      <header className="flex justify-center gap-5">
-        <MonthPicker year={year} month={month} changeYear={setYear} changeMonth={setMonth} />
-      </header>
-      <ul className="px-6 grid grid-cols-7 gap-[20px] justify-items-center text-label-1-b">
-        {DAYS.map((day) => (
-          <li key={day}>{day}</li>
-        ))}
-      </ul>
-      <ul className="px-6 grid grid-cols-7 gap-[20px]">
-        {dateList.map((date: Date) => (
-          <DateBox
-            key={date.toISOString()}
-            date={date}
-            isCurrentMonth={isCurrentMonth(date)}
-            isToday={isToday(date)}
-            isSunday={isSunday(date)}
-          />
-        ))}
-      </ul>
-      <button
-        onClick={onClickHandler}
-        className="flex flex-col w-20 p-[20px] gap-[20px] rounded-[20px] shadow-4 bg-white"
-      >
-        Home
-      </button>
-    </div>
+    <>
+      <TopNavigation onClickBack={routeBack} backGroundColor="transparent" />
+      <main className="flex flex-col gap-6 pt-12">
+        <header className="flex justify-center gap-5">
+          <MonthPicker year={year} month={month} changeYear={setYear} changeMonth={setMonth} />
+        </header>
+        <ul className="px-6 grid grid-cols-7 gap-[20px] justify-items-center text-label-1-b">
+          {DAYS.map((day) => (
+            <li key={day}>{day}</li>
+          ))}
+        </ul>
+        <ul className="px-6 grid grid-cols-7 gap-[20px]">
+          {dateList.map((date: Date) => (
+            <DateBox
+              key={date.toISOString()}
+              date={date}
+              isCurrentMonth={isCurrentMonth(date)}
+              isToday={isToday(date)}
+              isSunday={isSunday(date)}
+            />
+          ))}
+        </ul>
+      </main>
+    </>
   );
 }
 
