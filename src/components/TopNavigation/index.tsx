ebef609a236ebac1184f176d9bd0356512d85ad9
hyejun <hyejunkkim228@gmail.com>
2025-02-19 15:34:37 +0900

    feat: 스타일 변경

--- src/components/TopNavigation/index.tsx
+++ src/components/TopNavigation/index.tsx
@@ -1,10 +1,20 @@
 import ChevronLeftIcon from '@/assets/svg/chevron-left.svg';
 
-function TopNavigation({ children, onClickBack }: { children: React.ReactNode; onClickBack: () => void }) {
+interface TopNavigationProps { 
+  children: React.ReactNode;
+  onClickBack: () => void;
+  backGroundColor?: string;
+}
+
+function TopNavigation({
+  children,
+  onClickBack,
+  backGroundColor = 'bg-white',
+}: TopNavigationProps) {
   return (
-    <header className="fixed top-0 left-0 w-full justify-between flex py-3 px-4 z-50 bg-white">
+    <header className={`fixed top-0 left-0 w-full justify-between flex py-3 px-4 z-50 ${backGroundColor}`}>
       <button type="button" onClick={onClickBack}>
-        <ChevronLeftIcon />
+        <ChevronLeftIcon width={24} height={24}/>
       </button>
       {children}
     </header>
