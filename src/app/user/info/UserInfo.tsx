2b79a1441aec90681dc20ae08aa26425997c175a
hyejun <hyejunkkim228@gmail.com>
2025-03-19 22:35:36 +0900

    feat: 정보수정 api 연결

---
+++ src/app/user/info/UserInfo.tsx
@@ -0,0 +1,59 @@
+import { suitFont } from '@/styles/font';
+import { cookies } from 'next/headers';
+import ClientTopNavigation from './ClientTopNavigation';
+
+interface UsersInfo {
+  id: number;
+  name: string;
+  nickname: string;
+  birth_date: string;
+  email: string;
+  profile_image_url: string;
+  gender: string;
+  is_public: boolean;
+  follower_count: number;
+  following_count: number;
+  created_at: string;
+  updated_at: string;
+  is_deleted: boolean;
+}
+
+async function UserInfo() {
+  const cookieHeader = cookies().toString();
+
+  const response = await fetch('https://api.momenty.co.kr/users/me', {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json', Cookie: cookieHeader },
+  });
+
+  if (!response.ok) {
+    throw new Error('데이터를 가져오지 못했습니다.');
+  }
+
+  const { nickname, birth_date, email }: UsersInfo = await response.json();
+
+  return (
+    <>
+      <ClientTopNavigation />
+      <main className={`flex flex-col items-center gap-5 mt-12 ${suitFont.className}`}>
+        <div className="w-[72px] h-[72px] bg-indigo-50 rounded-[12px] my-[15px]" />
+        <section className="flex flex-col w-full">
+          <div className="flex justify-between align-center px-8 py-2">
+            <span className="text-body-1-m text-indigo-300">이름</span>
+            <span className="text-body-1-m text-indigo-300">{nickname}</span>
+          </div>
+          <div className="flex justify-between align-center px-8 py-2">
+            <span className="text-body-1-m text-indigo-300">생년월일</span>
+            <span className="text-body-1-m text-indigo-300">{birth_date}</span>
+          </div>
+          <div className="flex justify-between align-center px-8 py-2">
+            <span className="text-body-1-m text-indigo-300">이메일 주소</span>
+            <span className="text-body-1-m text-indigo-300">{email}</span>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default UserInfo;
