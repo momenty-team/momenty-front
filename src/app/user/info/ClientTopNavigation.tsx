82acbc8efa19482953b3ace06e42044bf91fa643
hyejun <hyejunkkim228@gmail.com>
2025-03-25 02:28:21 +0900

    fix: 불필요한 코드 삭제

--- src/app/user/info/ClientTopNavigation.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import TopNavigation from '@/common/components/TopNavigation';
-import { postMessageToWebView } from '@/utils';
-
-export default function ClientTopNavigation() {
-  return <TopNavigation onClickBack={() => postMessageToWebView({ route: 'goBack' })} backGroundColor="transparent" />;
-}
