b211c979a58eda97f17fc7c0bbc03b101e70ac39
hyejun <hyejunkkim228@gmail.com>
2025-05-21 22:46:02 +0900

    feat: 수면 데이터 chart 추가

--- src/app/healthkit-detail/sleep/page.tsx
+++ src/app/healthkit-detail/sleep/page.tsx
@@ -1,37 +1,75 @@
-'use  client';
+'use client';
 
-// import { useState } from 'react';
+import { useState } from 'react';
+import { SleepChart } from '@/common/components/Chart';
+import useAppMessage from '@/common/hooks/useAppMessage';
+import type { CustomHealthSleepValue } from '@/types';
 
-// function getWeekdayFromISOString(isoString: string): string {
-//   const date = new Date(isoString);
+export const calculateTotalSleepTime = (sleepSamples: CustomHealthSleepValue[]) => {
+  const sleepOnly = sleepSamples
+    .filter((sample) => sample.value !== 'AWAKE')
+    .map((sample) => ({
+      start: new Date(sample.startDate).getTime(),
+      end: new Date(sample.endDate).getTime(),
+    }));
 
-//   const weekdays = ['일', '월', '화', '수', '목', '금', '토'];
+  // 1. start 기준으로 정렬
+  const sorted = sleepOnly.sort((a, b) => a.start - b.start);
 
-//   return weekdays[date.getDay()];
-// }
+  // 2. 겹치는 구간 병합
+  const merged: { start: number; end: number }[] = [];
+  for (const interval of sorted) {
+    if (merged.length === 0 || merged[merged.length - 1].end < interval.start) {
+      merged.push({ ...interval });
+    } else {
+      merged[merged.length - 1].end = Math.max(merged[merged.length - 1].end, interval.end);
+    }
+  }
+
+  // 3. 병합된 구간들의 총 수면 시간 계산
+  const totalMillis = merged.reduce((sum, interval) => sum + (interval.end - interval.start), 0);
+
+  const totalMinutes = Math.floor(totalMillis / 1000 / 60);
+  const hours = Math.floor(totalMinutes / 60);
+  const minutes = totalMinutes % 60;
+
+  return `${String(hours).padStart(2, '0')}시간 ${String(minutes).padStart(2, '0')}분`;
+};
 
 function HealthKitSleepDetailPage() {
-  // const [data, setData] = useState<any>(null);
-  // const [snapIndex, setSnapIndex] = useState(0);
-  // const startDates = data?.sleepAnalysis;
-  // const startDay = startDates?.[0].startDate ?? null;
-  // const endDay = startDates?.[startDates.length - 1].startDate ?? null;
+  const [data, setData] = useState<CustomHealthSleepValue[] | null>(null);
+  const [snapIndex, setSnapIndex] = useState(0);
 
-  // const sleepAnalysisLabels = data?.sleepAnalysis?.map(({ startDate }) => getWeekdayFromISOString(startDate)) || [];
-  // const sleepAnalysisDatasets = [
-  //   {
-  //     label: '수면 분석',
-  //     data: data?.sleepAnalysis?.map(({ value }) => value) || [],
-  //     backgroundColor: '#69B1FF',ㄴ
-  //     borderColor: '#69B1FF',
-  //     borderWidth: 1,
-  //   },
-  // ];
+  useAppMessage(({ bottomSheet, healthKitData }) => {
+    if (healthKitData && healthKitData.sleepSamples) {
+      setData(healthKitData.sleepSamples);
+    }
+
+    if (bottomSheet?.state === 'close') {
+      setSnapIndex(-1);
+    }
+
+    if (bottomSheet?.state === 'hold' && bottomSheet.snapIndex) {
+      setSnapIndex(bottomSheet.snapIndex);
+    }
+  }, []);
 
   return (
-    <div>
-      <h1>수면 분석</h1>
-    </div>
+    <>
+      <header className="fixed w-full flex flex-col items-center bg-white top-0 h-[48px] pb-2">
+        <div className="text-indigo-700 text-body-3-sb">수면</div>
+        <span className="text-body-4-m text-indigo-300">{calculateTotalSleepTime(data ?? [])}</span>
+      </header>
+
+      <section
+        className="fixed top-[48px] w-full flex flex-col align-center overflow-y-scroll transition-[height] duration-300 gap-12 px-5"
+        style={{
+          height: snapIndex > 1 ? `calc(100vh - 60px)` : 'calc(100vh / 9 * 5 - 72px)',
+        }}
+      >
+        {data && <SleepChart sleepSamples={data} />}
+      </section>
+    </>
   );
 }
 
--- src/common/components/Chart/index.tsx
+++ src/common/components/Chart/index.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React from 'react';
 import {
   Chart as ChartJS,
@@ -114,4 +116,96 @@ export function BarChart({ data }: BarChartProps) {
 
 export function LineChart({ data }: BarChartProps) {
   return <Line options={options} data={data} />;
+}
+
+type SleepSample = {
+  startDate: string;
+  endDate: string;
+  value: 'AWAKE' | 'REM' | 'CORE' | 'DEEP' | 'INBED';
+};
+
+const VALUE_TITLE_MAP = {
+  AWAKE: '비수면',
+  REM: 'REM 수면',
+  CORE: '코어 수면',
+  DEEP: '깊은 수면',
+  INBED: '취침시간',
+};
+
+const sleepColorMap: Record<SleepSample['value'], string> = {
+  AWAKE: '#ff6b6b',
+  REM: '#90e0ef',
+  CORE: '#0077b6',
+  DEEP: '#03045e',
+  INBED: '#6a0dad',
+};
+
+const sleepOrder: SleepSample['value'][] = ['AWAKE', 'REM', 'CORE', 'DEEP'];
+
+export function SleepChart({ sleepSamples }: { sleepSamples: SleepSample[] }) {
+  const start = Math.min(...sleepSamples.map((s) => new Date(s.startDate).getTime()));
+  const end = Math.max(...sleepSamples.map((s) => new Date(s.endDate).getTime()));
+  const totalMillis = end - start;
+
+  const sleepByLevel = sleepSamples.reduce<Record<SleepSample['value'], SleepSample[]>>(
+    (acc, sample) => {
+      if (!acc[sample.value]) {
+        acc[sample.value] = [];
+      }
+      acc[sample.value].push(sample);
+      return acc;
+    },
+    { AWAKE: [], REM: [], CORE: [], DEEP: [], INBED: [] }
+  );
+
+  const timeLabels = Array.from({ length: 4 }, (_, i) => {
+    const time = new Date(start + (totalMillis * i) / 3);
+    return time.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false });
+  });
+
+  return (
+    <div className="relative w-full flex flex-col gap-1" style={{ borderLeft: '1px solid #E8EBEF' }}>
+      {sleepOrder.map((level) => (
+        <div key={level} className="relative w-full min-h-14 h-14 z-10" style={{ borderBottom: '1px solid #E8EBEF' }}>
+          <div className="absolute left-1 text-caption-2-m text-indigo-200">{VALUE_TITLE_MAP[level]}</div>
+          <div className="relative w-full h-full">
+            {sleepByLevel[level].map((sample, i) => {
+              const sampleStart = new Date(sample.startDate).getTime();
+              const sampleEnd = new Date(sample.endDate).getTime();
+
+              const left = ((sampleStart - start) / totalMillis) * 100;
+              const width = ((sampleEnd - sampleStart) / totalMillis) * 100;
+
+              return (
+                <div
+                  key={`${level}-${i}`}
+                  className="absolute rounded opacity-85 bottom-1.5"
+                  style={{
+                    left: `${left}%`,
+                    width: `${width}%`,
+                    height: `24px`,
+                    backgroundColor: sleepColorMap[sample.value],
+                  }}
+                />
+              );
+            })}
+          </div>
+        </div>
+      ))}
+      <div className="flex justify-between text-label-1-m text-indigo-100 w-full">
+        {timeLabels.map((_, idx) => (
+          <div
+            key={`line-${idx}`}
+            className="absolute top-0 h-[calc(100%-20px)] border-l border-dashed border-indigo-50"
+            style={{ left: `${(idx / 3) * 100}%`, display: `${idx === 0 && 'none'}` }}
+          />
+        ))}
+        <div className="flex justify-between text-caption-2-m text-indigo-100 w-full">
+          {timeLabels.map((label, idx) => (
+            <div key={`label-${idx}`}>{label}</div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
